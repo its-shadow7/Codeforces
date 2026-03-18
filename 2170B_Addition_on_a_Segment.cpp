828a2ae0e68b0a667ef048fc4b281bf674b6859e
Surya Pratap Singh Rawat <140951817+its-shadow7@users.noreply.github.com>
2026-03-19 00:38:48 +0530

    Sync Codeforces: 2170B - Addition on a Segment [C++20 (GCC 13-64)]

---
+++ 2170B_Addition_on_a_Segment.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#pragma GCC optimize ("O2")
+ 
+#define ll long long
+#define int long long
+#define nn '\n'
+#define vi vector<int>
+#define vii vector<vector<int>>
+#define all(x) x.begin(), x.end()
+#define read(v) for(auto &x : v) cin >> x
+#define show(v) for(auto &x : v) cout << x << " " 
+#define pb push_back
+ 
+const int mod = 1e9+7;
+ 
+bool testcases = 1;
+ 
+void solve(){
+    int n;
+    cin  >> n;
+    vi v(n,0);
+    read(v);
+    int summ = accumulate(all(v),0ll);
+    int ans1 = summ - n+1;
+    int zero = count(all(v),0ll);
+    cout << min(ans1,n-zero) << nn;
+}
+ 
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int test = 1;
+    if(testcases) cin >> test;
+    while(test--){
+        solve();
+    }
+    return 0;
+}
