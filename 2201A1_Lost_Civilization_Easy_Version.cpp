7abce0093317a356c58cf4d9616723a9b07c898c
Surya Pratap Singh Rawat <140951817+its-shadow7@users.noreply.github.com>
2026-03-25 12:37:18 +0530

    Sync Codeforces: 2201A1 - Lost Civilization (Easy Version) [C++23 (GCC 14-64, msys2)]

---
+++ 2201A1_Lost_Civilization_Easy_Version.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#pragma GCC optimize ("O2")
+ 
+#define ll long long
+#define int long long
+#define nn '\n'
+#define vi vector<int>
+#define vii vector<vector<int>>
+#define all(x) x.begin(), x.end()
+#define read(v) for(auto &x : v) cin >> x
+#define show(v) for(auto &x : v) cout << x << " " 
+#define pb push_back
+ 
+const int mod = 1e9+7;
+ 
+bool testcases = 1;
+ 
+void solve(){
+    int n;
+    cin >>n;
+    vi v(n,0);
+    read(v);
+    stack <int> st;
+    
+    int ans =0;
+    for (int i=0;i<n;i++){
+ 
+        while(!st.empty() and st.top()!=v[i]-1){
+            st.pop();
+        }
+ 
+        if (st.empty()){
+            ans++;
+        }
+        st.push(v[i]);
+ 
+    }
+    cout << ans << nn;
+}
+ 
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int test = 1;
+    if(testcases) cin >> test;
+    while(test--){
+        solve();
+    }
+    return 0;
+}
