5613b19a1ae95c881a5ad28331879d2012b03651
Surya Pratap Singh Rawat <140951817+its-shadow7@users.noreply.github.com>
2026-03-25 12:37:44 +0530

    Sync Codeforces: 1537D - Deleting Divisors [C++23 (GCC 14-64, msys2)]

---
+++ 1537D_Deleting_Divisors.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#pragma GCC optimize ("O2")
+ 
+#define ll long long
+#define int long long
+#define nn '\n'
+#define vi vector<int>
+#define vii vector<vector<int>>
+#define all(x) x.begin(), x.end()
+#define read(v) for(auto &x : v) cin >> x
+#define show(v) for(auto &x : v) cout << x << " " 
+#define pb push_back
+ 
+const int mod = 1e9+7;
+ 
+bool testcases = 1;
+ 
+void solve(){
+    int n;
+    cin >> n;
+ 
+    int count = 0;
+    if (n&1) cout << "Bob" << nn;
+    else{
+        while (!(n&1)) {
+            count++;
+            n = n>>1;
+        }
+        if (n>1 or !(count&1)) cout << "Alice" << nn;
+        else cout<< "Bob" << nn;
+    }
+ 
+}
+ 
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int test = 1;
+    if(testcases) cin >> test;
+    while(test--){
+        solve();
+    }
+    return 0;
+}
