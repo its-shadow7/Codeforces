f9b7c901f6ef29179120d47bdc16cf133d1004d8
Surya Pratap Singh Rawat <140951817+its-shadow7@users.noreply.github.com>
2026-03-28 20:19:39 +0530

    Sync Codeforces: 18C - Stripe [C++23 (GCC 14-64, msys2)]

---
+++ 18C_Stripe.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#pragma GCC optimize ("O2")
+ 
+#define ll long long
+#define int long long
+#define nn '\n'
+#define vi vector<int>
+#define vii vector<vector<int>>
+#define all(x) x.begin(), x.end()
+#define read(v) for(auto &x : v) cin >> x
+#define show(v) for(auto &x : v) cout << x << " " 
+#define pb push_back
+ 
+const int mod = 1e9+7;
+ 
+bool testcases = 0;
+ 
+void solve(){
+    int n;
+    cin >> n;
+    vi v(n,0);
+    read(v);
+    int ans = 0;
+    for(int i =1;i<n;i++) v[i]=v[i-1]+v[i];
+ 
+    for(int i =0;i<n-1;i++){
+        if(v[i] == v[n-1]-v[i]) ans++;
+    }
+    cout << ans << nn;
+}
+ 
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int test = 1;
+    if(testcases) cin >> test;
+    while(test--){
+        solve();
+    }
+    return 0;
+}
